d8ca5bf99b167f86fdd8fde0cb8274ce2d1283ba
Asaev Maksim <maksimasaev@gmail.com>
2013-09-12 11:37:24 +0600

    Update lab1_1.cpp

--- lab1_1.cpp
+++ lab1_1.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	denominator = counter = 1; //Устанавливаем начальные значение
 
 	printf("Uveli4ivaem i otobrajaem na ekrane\n");
-	printf("%d\n",counter++);
+	printf("%d\n",++counter);
 	printf("Vi4islyaem zna4enie Counter/Denominator = ");
 	printf("%d\n",counter/denominator);
 	printf("" ,counter);
