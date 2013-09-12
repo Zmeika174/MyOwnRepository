f7cd601f1033defd9cfceeef67877c0957ca3a4c
Zmeika174 <maksimasaev@gmail.com>
2013-09-12 11:10:51 +0600

    lab1_1.cpp
    Eto 1 4ast' 1 laboratornoi raboti. Vrode bi

---
+++ lab1_1.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+int main(int argc, char *argv[])
+{
+
+	int counter, denominator;
+
+	denominator = counter = 1; //Устанавливаем начальные значение
+
+	printf("Uveli4ivaem i otobrajaem na ekrane\n");
+	printf("%d\n",counter++);
+	printf("Vi4islyaem zna4enie Counter/Denominator = ");
+	printf("%d\n",counter/denominator);
+	printf("" ,counter);
+
+}
